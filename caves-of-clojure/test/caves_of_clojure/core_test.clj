28e846c12436be380710932610a8de4ad1534535
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2016-08-26 23:24:02 +0200

    caves-of-clojure: Lein default template.

---
+++ caves-of-clojure/test/caves_of_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns caves-of-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [caves-of-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
